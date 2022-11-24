769b84aa0c2868585c90eba962db48f4ba21698c
kirilgoloveiko <snikerz.kg@gmail.com>
2022-11-24 18:48:51 +0300

    homework 8

--- lesson_8/task1.py
+++ lesson_8/task1.py
@@ -1,14 +1,15 @@
 import string
 
 
-def CamelCase(str):
+def camelCase(str):
     signs = list(string.punctuation)
     for el in str:
         if el in signs:
             str = str.replace(el, ' ')
     temp_lst = str.split()
-    list_1 = [i.capitalize() for i in temp_lst]
+    list_1 = [i.capitalize() if temp_lst.index(i) > 0 else i for i in temp_lst]
     return ''.join(list_1)
 
 
-# print(CamelCase('The-stealth!*&warrior'))
+print(camelCase('the-stealth_warrior'))
+
